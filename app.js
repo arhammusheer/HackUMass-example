834fca8071490fb9cfd77d816a4ef5655a039cb9
arhammusheer <arhammusheer@gmail.com>
2021-11-02 06:19:26 -0400

    okeh

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const morgan = require("morgan");
 const indexRouter = require("./routes/index");
 const userRouter = require("./routes/user");
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = express(); // create an instance of express
+const PORT = process.env.PORT || 3000; // process.env.PORT imports the port from the environment variables
 
-app.use(morgan("dev"));
-app.use("/", indexRouter);
-app.use("/users", userRouter)
+app.use(morgan("dev")); // log every request to the console
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.listen(PORT, () => {
-  console.info(`Server started on port ${PORT}`);
+app.use("/", indexRouter); // home API
+app.use("/users", userRouter); // user API
+
+app.listen(PORT, () => { // listen on port 3000 or the port specified in the environment variables
+  console.log(`Server started on port ${PORT}`); 
 });
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 
+// @route   GET /user
+// @desc    Get all users
+// @access  Public
 router.get("/", (req, res) => {
   const name = req.query.name; // get the name from the query string
   let users = fs.readdirSync("./data/users"); // get all the users from the data folder
@@ -27,10 +30,111 @@ router.get("/", (req, res) => {
       });
     }
   }
-  users = users.map((users) => { 
+  users = users.map((users) => {
     return users.slice(0, -5); // slice the users to remove the .json extension
   });
   return res.json({ users }); // if the name is empty, return all the users
+});
+
+// @route   GET /user/:id
+// @desc    Get a user
+// @access  Public
+router.get("/:name", (req, res) => {
+  const name = req.params.name; // get the id from the params
+  try {
+    let user = fs.readFileSync(`./data/users/${name}.json`); // get the user from the data folder
+    user = JSON.parse(user); // parse the user to JSON
+    return res.json({ user }); // return the user
+  } catch (error) {
+    // if the user is not found
+    return res.json({
+      error: "User not found", // return an error
+    });
+  }
+});
+
+// @route   POST /user
+// @desc    Create a user
+// @access  Public
+router.post("/", (req, res) => {
+  const name = req.body.name; // get the name from the body
+  const message = req.body.message; // get the message from the body
+
+  if (name && message) {
+    // if the name and message are not empty
+    try {
+      if (fs.existsSync(`./data/users/${name}.json`)) {
+        // if the user already exists
+        throw "User already exists";
+      }
+      fs.writeFileSync(
+        `./data/users/${name}.json`,
+        JSON.stringify({
+          // write the user to the data folder
+          name,
+          message,
+        })
+      ); // write the message to the user file
+      return res.json({
+        // return a success message
+        message: "User created",
+        user: {
+          name,
+          message,
+        },
+      });
+    } catch (error) {
+      // if the user already exists
+      return res.json({
+        // return an error
+        error: "User already exists",
+      });
+    }
+  } else {
+    // if the name or message is empty
+    return res.json({
+      // return an error
+      error: "Please enter a name and message",
+    });
+  }
+});
+
+// @route   PATCH /user/:id
+// @desc    Update a user
+// @access  Public
+router.patch("/:name", (req, res) => {
+  const name = req.params.name; // get the id from the params
+  const message = req.body.message; // get the message from the body
+
+  if (name && message) {
+    // if the name and message are not empty
+    try {
+      let user = fs.readFileSync(`./data/users/${name}.json`); // get the user from the data folder
+      user = JSON.parse(user); // parse the user to JSON
+      user.message = message; // update the message
+      fs.writeFileSync(`./data/users/${name}.json`, JSON.stringify(user)); // write the user to the data folder
+      return res.json({
+        // return a success message
+        message: "User updated",
+        user: {
+          name,
+          message,
+        },
+      });
+    } catch (error) {
+      // if the user is not found
+      return res.json({
+        // return an error
+        error: "User not found",
+      });
+    }
+  } else {
+    // if the name or message is empty
+    return res.json({
+      // return an error
+      error: "Please enter a name and message",
+    });
+  }
 });
 
 module.exports = router;
